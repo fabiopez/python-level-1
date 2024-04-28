f0d6be16abf621a73bc6e2a3ad7133ee37222488
fabiopez <167480869+fabiopez@users.noreply.github.com>
2024-04-28 22:23:35 +0100

    Add files via upload

---
+++ Unit Converter.py
@@ -0,0 +1,10 @@
+print("Do you want to converts kilograms into pounds?")
+kg = float(input("Please enter the quantity in kilograms you want to convert:"))
+pounds = kg * 2.20462
+if kg == float(kg):
+  kg == float(kg)
+else:
+  kg = int(kg)
+print(f"{kg} kg is equivalent to {pounds} pounds")
+
+# I couldn't workout how to handle int values nor strings
